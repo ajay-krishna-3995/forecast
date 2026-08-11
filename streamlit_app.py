9e556a88fe8fd563c58619650e4a6612a16c661e
ajay-krishna-3995 <krishnaajay886@gmail.com>
2026-08-11 16:18:12 +0530

    Rename 'Weather update' tab to 'Weather summary'

--- streamlit_app.py
+++ streamlit_app.py
@@ -489,7 +489,7 @@ location_name = get_location_name(st.session_state.lat, st.session_state.lon, se
 # -------------------------------------------------------------------------------
 # WORKSPACE LAYOUT & GRAPHICS RENDERING
 # -------------------------------------------------------------------------------
-tab_home, tab_meteogram, tab_grib_analysis, tab_research, tab_weather_update = st.tabs(["🏡 Home", "📈 Meteogram", "Rainfall", "Research","Weather update"])
+tab_home, tab_meteogram, tab_grib_analysis, tab_research, tab_weather_update = st.tabs(["🏡 Home", "📈 Meteogram", "Rainfall", "Research","Weather summary"])
 
 # --- 1. HOME TAB ---
 with tab_home:
